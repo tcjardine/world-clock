46f762e79b892d9dac393e0e1dcff970af6b36b4
tcjardine <112444700+tcjardine@users.noreply.github.com>
2023-06-21 18:12:38 -0700

    Added function for the select (city)

--- javascript/index.js
+++ javascript/index.js
@@ -15,5 +15,27 @@ function updateTime() {
   romeDateElement.innerHTML = romeTime.format("dddd MMMM Do YYYY");
   romeTimeElement.innerHTML = romeTime.format("h:mm:ss A");
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">
+            ${cityTime.format("MMMM Do YYYY")}
+            </div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss A")}</div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElemenet = document.querySelector("#city");
+
+citiesSelectElemenet.addEventListener("change", updateCity);
